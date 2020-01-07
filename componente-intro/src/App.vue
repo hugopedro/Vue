ffbd7599da7cf60a1bd7a191c5f4568094f9fc9a
hugopedro <hugopedro098@gmail.com>
2020-01-07 16:58:45 -0300

    Inicio Componentes Vue

---
+++ componente-intro/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app"> <!-- ja que mais de um elemento precisa de div --->
+    <h1>Contadores</h1>
+    <app-contadores />  
+  </div>
+</template>
+<!--103. Criar projeto usando Vue CLI -->
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
---
+++ componente-intro/src/Contadores.vue
@@ -0,0 +1,18 @@
+<!-- 105. Usando componentes -->
+
+<template>
+  <div class="contadores">
+      <app-contador />
+  </div>
+</template>
+
+<script>
+import Contador from './Contador.vue'
+export default {
+    components: { 'app-contador': Contador }
+}
+</script>
+
+<style>
+
+</style>
