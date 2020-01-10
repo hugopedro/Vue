249617bf133c81335be0c0dac31f011ae2bc1699
hugopedro <hugopedro098@gmail.com>
2020-01-10 15:55:52 -0300

    Usando Props no Componente Filho

--- componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
+++ componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -2,7 +2,7 @@
     <div class="componente">
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
-        <p>Nome do Usuario: <strong> {{ nome }}</strong></p>
+        <p>Nome do Usuario: <strong> {{ inverterNome() }}</strong></p>
     </div>
 </template>
 
@@ -13,7 +13,12 @@ export default {
     * entao o {{ nome }} da linha 5 vai vir apartir da propiedade nome abaixo e passa-se a propiedade
     * no momento que cria a Tag de um componente, no caso no Usuario.Vue linha 8
     * A comunicação do componente pai e do componente filho é por causa das propiedades props */
-    props: ['nome']
+    props: ['nome'],
+    methods: {
+        inverterNome() {
+            return this.nome.split('').reverse().join('')
+        }
+    }
 }
 </script>
 
