a40ddc4669bf73421fc64a8a8ecdc2ffb8a36275
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2020-07-12 14:27:40 -0300

    Componente Lista de tarefas #02

--- projeto02-tarefas/tarefas/src/components/TaskGrid.vue
+++ projeto02-tarefas/tarefas/src/components/TaskGrid.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="task-grid">
-      <template>
+      <template v-if="tasks.length">
           <!--nao pode aceitar duas tarefas com o mesmo nome entao vou por nome como chave -->
           <Task v-for="task in tasks" :key="task.name" :task="task"></Task>
           <!--gera as divs -->
       </template>
+      <p v-else class="no-task">Sua vida está em dia :)</p>
   </div>
 </template>
 
@@ -20,5 +21,18 @@ export default {
 </script>
 
 <style>
+    .task-grid {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
 
+    .task-grid .task {
+        margin: 10px;
+    }
+
+    .no-task {
+        color: #AAA;
+        font-size: 1.7rem;
+        }
 </style>
