703a92af56e15f27dfb9f1643ac5137477b64228
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2020-07-12 12:29:06 -0300

    149 Projeto #02 - Tarefas (TODO) e Componente Lista de tarefas #01

---
+++ projeto02-tarefas/tarefas/src/components/TaskGrid.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="teste">
+      <template>
+          <!--nao pode aceitar duas tarefas com o mesmo nome entao vou por nome como chave -->
+          <div v-for="task in tasks" :key="task.name">{{ task.name }}</div>
+          <!--gera as divs -->
+      </template>
+  </div>
+</template>
+
+<script>
+export default {
+    props: { type: Array, required: true }
+}
+</script>
+
+<style>
+
+</style>
