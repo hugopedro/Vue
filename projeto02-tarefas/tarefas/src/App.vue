f37c6bd32d2cfe8e9da28e176caca484fc33426c
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2020-07-13 13:03:54 -0300

    159 Usando Local Storage

--- projeto02-tarefas/tarefas/src/App.vue
+++ projeto02-tarefas/tarefas/src/App.vue
@@ -32,6 +32,15 @@ export default {
 				return Math.round(done / total * 100) || 0
 			}
 	},
+	watch: {
+		tasks: {
+			deep: true, //monitora os elementos pra qd sair da pagina continuarem lá
+			handler() {
+				localStorage.setItem('tasks', JSON.stringify(this.tasks))
+			//está pegando a lista de tasks que é this.tasks e convertendo pra uma string e setando no localStorage apartir da chave tasks
+			}
+		}
+	},
 	methods: {
 		addTask(task) {
 			const sameName = t => t.name === task.name
@@ -50,6 +59,11 @@ export default {
 		toggleTaskState(i) { //se tiver pendente ele coloca falso se nao verdadeiro
 			this.tasks[i].pending = !this.tasks[i].pending
 		}
+	},
+	created() { //funcao de ciclo de vida para as tasks sobreviverem ao sair da pagina
+		const json = localStorage.getItem('tasks')
+		this.tasks = JSON.parse(json) || []
+		this.tasks = Array.isArray(array) ? array : []
 	}
 }
 </script>
