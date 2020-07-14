9dfa96bb6c77ea0cf65ee8074641096c50ff4f30
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2020-07-14 15:04:30 -0300

    193 criando um filtro local

--- filtros-mixins-exercicios/src/App.vue
+++ filtros-mixins-exercicios/src/App.vue
@@ -1,11 +1,27 @@
 <template>
 	<div id="app">
 		<h1>Filtros & Mixins</h1>
+		<hr>
+		<p>{{ cpf | cpf }}</p> <!-- o pipe | faz mostrar com os filtros, o da esquerda é o valor bruto e segundo 'arrumado'-->
 	</div>
 </template>
 
 <script>
 export default {
+	filters: {
+		cpf(valor) { //filtro é uma funcao que recebe um valor e transforma e retorna
+			const arr = valor.split('')
+			arr.splice(3, 0, '.')
+			arr.splice(7, 0, '.')
+			arr.splice(11, 0, '-')
+			return arr.join('') // array transformado em string
+		}
+	},
+	data() {
+		return {
+			cpf: '25987456988'
+		}
+	}
 
 }
 </script>
