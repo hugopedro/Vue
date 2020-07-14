4cd96e8f8e66df79e12af958069e39a816cda6e1
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2020-07-14 11:24:46 -0300

    180 Usando diretivas personalizadas

---
+++ formulario-desafio/src/components/NomeCompleto.vue
@@ -0,0 +1,17 @@
+<template>
+	<Rotulo nome="Nome completo">
+		<input type="text" v-model="usuario.nomeCompleto">
+	</Rotulo>
+</template>
+
+<script>
+
+import Rotulo from './Rotulo.vue'
+export default {
+    components: { Rotulo },
+}
+</script>
+
+<style>
+
+</style>
