816881000ede5182e527ee21e34804fe88888351
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2020-07-14 15:49:02 -0300

    196 Duplicando os codigos para usar os Mixins

--- filtros-mixins-exercicios/src/App.vue
+++ filtros-mixins-exercicios/src/App.vue
@@ -4,11 +4,23 @@
 		<hr>
 		<p>{{ cpf | cpf | inverter }}</p> <!-- o pipe | faz mostrar com os filtros, o da esquerda é o valor bruto e segundo 'arrumado'-->
 		<input type="text" :value="cpf | cpf">
+		<hr>
+		<Frutas />
+		<hr>
+		<div>
+			<ul>
+				<li v-for="fruta in frutas" :key="fruta"> {{ fruta }}</li>
+			</ul>
+			<input type="text" v-model="fruta" @keydown.enter="add">
+ 		</div>
 	</div>
 </template>
 
 <script>
+import Frutas from './Frutas.vue'
 export default {
+
+	components: { Frutas },
 	filters: {
 		cpf(valor) { //filtro é uma funcao que recebe um valor e transforma e retorna
 			const arr = `${valor}`.split('')
@@ -20,9 +32,17 @@ export default {
 	},
 	data() {
 		return {
-			cpf: '25987456988'
+			cpf: '25987456988',
+			fruta: '',
+            frutas: ['banana', 'maça', 'laranja']
 		}
-	}
+	},
+	methods: {
+        add() {
+            this.frutas.push(this.fruta)
+            this.fruta = ''
+        }
+    }
 
 }
 </script>
