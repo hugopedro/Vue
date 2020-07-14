893ad9961b20b3ea8a162a61597c06e300f030af
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2020-07-14 12:23:33 -0300

    185 registrando diretivas localmente

--- diretivas-exercicios/src/App.vue
+++ diretivas-exercicios/src/App.vue
@@ -6,6 +6,9 @@
 		<hr>
 		<p v-destaque:fundo.atrasar="'lightblue'">Usando diretiva personalizada</p> <!-- se for passar string tem que usar aspas simples -->
 		<p v-destaque.atrasar="cor">Usando diretiva personalizada</p>
+		<hr>
+		<p v-destaque-local:fundo.atrasar="'lightblue'">Usando diretiva personalizada</p> <!-- se for passar string tem que usar aspas simples -->
+		<p v-destaque-local.atrasar="cor">Usando diretiva personalizada</p>
 
 		<!-- <p v-teste:argumento.mod1.mod2.mod3="'valor'"></p> -->
 
@@ -16,6 +19,25 @@
 
 <script>
 export default {
+	components: { },
+	directives: {
+		'destaque-local' : {
+			bind(el, binding, vnode) {
+				// el.style.backgroundColor = 'lightgreen'
+
+				let atraso = 0
+				if (binding.modifiers['atrasar']) atraso = 3000 //usando modificador pra atrasar
+
+				setTimeout(() => {
+					if (binding.arg === 'fundo') {
+						el.style.backgroundColor = binding.value
+					} else {
+						el.style.color = binding.value
+					}
+				}, atraso )
+			}
+				}
+	},
 	data() {
 		return {
 			cor: 'red'
