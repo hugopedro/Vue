b2b2c671f9704d6e84a311324af299f8ae14eb43
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2020-07-15 19:17:54 -0300

    232 233 234 235 Conectando com servidor HTTP: criando instancia do axios, criando formulário e enviando POST

---
+++ http-exercicios/src/plugins/axios.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+// axios.defaults.baseURL = 'https://curso-vue-b6f16.firebaseio.com/'
+
+Vue.use({
+    install(Vue) {
+        // Vue.prototype.$http = axios
+           Vue.prototype.$http = axios.create({
+                baseURL : 'https://curso-vue-b6f16.firebaseio.com/'
+           })
+        
+    }
+})
