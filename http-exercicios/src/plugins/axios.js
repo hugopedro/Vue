2a580230d55b368362d7ad8bda43aaedb284eb9a
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2020-07-15 20:16:13 -0300

    240 Adicionando Headers Globais

--- http-exercicios/src/plugins/axios.js
+++ http-exercicios/src/plugins/axios.js
@@ -2,12 +2,17 @@ import Vue from 'vue'
 import axios from 'axios'
 
 // axios.defaults.baseURL = 'https://curso-vue-b6f16.firebaseio.com/'
+// axios.defaults.headers.common['Authorization'] = 'abc123'
+// axios.defaults.headers.get['Accepts'] = 'application/json'
 
 Vue.use({
     install(Vue) {
         // Vue.prototype.$http = axios
            Vue.prototype.$http = axios.create({
-                baseURL : 'https://curso-vue-b6f16.firebaseio.com/'
+                baseURL : 'https://curso-vue-b6f16.firebaseio.com/',
+                headers: {
+                    "Authorization" : "abc123"
+                }
            })
 
            Vue.prototype.$http.interceptors.request.use(config => {
