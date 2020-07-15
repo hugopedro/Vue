5d7b77bcb4c08d4179d2a1eaec2def25f49e8eeb
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2020-07-15 11:20:39 -0300

    214 Usando nomes e atributos dinamicos (bind)

--- animacoes-exercicios/src/App.vue
+++ animacoes-exercicios/src/App.vue
@@ -4,13 +4,23 @@
 		<hr>
 		<b-button variant="primary" class="mb-4"
 		@click="exibir = !exibir">Mostrar Mensagem</b-button> <!--faz com que exiba ou nao o botao -->
+<!--
+		<transition name="fade"> é importante por o nome aqui, se nao ia substituir por um simples v- , e os .fade da parte de baixo do css precisa disso-->
+	<!--		<b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
+		</transition> 
 
-		<transition name="fade"> <!--é importante por o nome aqui, se nao ia substituir por um simples v- , e os .fade da parte de baixo do css precisa disso-->
-			<b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
-		</transition>
+	<!--	<transition name="slide" type="animation"> <!--é importante por o nome aqui, se nao ia substituir por um simples v- , e os .fade da parte de baixo do css precisa disso-->
+	<!--		<b-alert variant="info" show v-show="exibir">{{ msg }}</b-alert>
+		</transition> -->
 
-		<transition name="slide"> <!--é importante por o nome aqui, se nao ia substituir por um simples v- , e os .fade da parte de baixo do css precisa disso-->
-			<b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
+		<hr>
+		<b-select v-model="tipoAnimacao" class="mb-4">
+			<option value="fade">Fade</option>
+			<option value="slide">Slide</option>
+		</b-select>
+
+		<transition :name="tipoAnimacao"> <!--é importante por o nome aqui, se nao ia substituir por um simples v- , e os .fade da parte de baixo do css precisa disso-->
+			<b-alert variant="info" show v-show="exibir">{{ msg }}</b-alert>
 		</transition>
 	</div>
 </template>
@@ -21,7 +31,8 @@ export default {
 	data() {
 		return {
 			msg: 'Uma mensagem de informação para o usuário!',
-			exibir: false
+			exibir: false,
+			tipoAnimacao: 'fade'
 		}
 	}
 }
@@ -54,14 +65,21 @@ export default {
 
 @keyframes slide-out {
 	from { transform: translateY(40px); }
+	to { transform: translateY(0); }
 }
 
 .slide-enter-active {
 	animation: slide-in 2s ease;
+	transition: opacity 2s;
+
 }
 
 .slide-leave-active {
 	animation: slide-in 2s ease;
+	transition: opacity 2s;
 }
 
+.slide-enter, .slide-leave-to {
+	opacity: 0;
+}
 </style>
