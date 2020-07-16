e7c8612a8102cd45eea862bf3d7d7857f561d3b2
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2020-07-16 12:09:55 -0300

    247  Introdução a rotas em uma aplicação VueJS

---
+++ rotas-exercicios/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<div id="app">
+		<h1>Rotas com VueRouter</h1>
+		<router-view /> <!-- isso fará com que seja carregado os componentes do router.js, e usará os paths definidos de lá-->
+	</div>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style>
+	#app {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+</style>
