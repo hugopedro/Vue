2318496cc879454f48998a652dffc8e445b086f4
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2020-07-16 13:22:21 -0300

    250 Estilizando o Link Ativo

--- rotas-exercicios/src/components/template/Menu.vue
+++ rotas-exercicios/src/components/template/Menu.vue
@@ -1,7 +1,17 @@
 <template>
   <nav class="menu">
+      <ul>
+         <router-link to="/" tag="li" active-class="active" exact><a>Inicio</a></router-link>  
+         <router-link to="/Usuario" tag="li" active-class="active" exact><a>Usuário</a></router-link> <!--equivale ao codigo comentado abaixo, é a melhor prática -->
+      </ul> <!--o active-class="active" exact funciona pra ao clicar na guia só a guia clicada ficar ativa usando o estilo, entao é importante -->
+      <!-- <ul>
+          <li>
       <router-link to="/">Inicio </router-link>   <!-- Cria um link e faz o processamento por tras-->
+          <!-- </li>
+          <li>
       <router-link to="/Usuario">Usuário</router-link>
+          </li>
+      </ul> -->
   </nav>
 </template>
 
