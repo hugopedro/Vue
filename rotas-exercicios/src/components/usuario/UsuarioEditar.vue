330a470c5ae50554bb5567d26ea263a6dbb7d01f
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2020-07-20 16:14:42 -0300

    269 Usando o evento beforeLeave para dar um pop-up ao sair

--- rotas-exercicios/src/components/usuario/UsuarioEditar.vue
+++ rotas-exercicios/src/components/usuario/UsuarioEditar.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="usuario-editar">
     <h3>Usuário Editar</h3>
-    <p><strong>Código: </strong> {{ id }} / {{ $route.params.id }}</p>
-    <p><strong>Completo: </strong> {{ $route.query.completo ? 'Sim' : 'Não' }}</p> <!-- pega os valores da query nos componentes que passaram a requisição-->
-    <p><strong>Língua:</strong> {{ $route.query.lingua }}</p>
-    <hr>
-    <button primario>Confirmar</button> <!-- protegendo rotas -->
+    <p>
+      <strong>Código:</strong>
+      {{ id }} / {{ $route.params.id }}
+    </p>
+    <p>
+      <strong>Completo:</strong>
+      {{ $route.query.completo ? 'Sim' : 'Não' }}
+    </p>
+    <!-- pega os valores da query nos componentes que passaram a requisição-->
+    <p>
+      <strong>Língua:</strong>
+      {{ $route.query.lingua }}
+    </p>
+    <hr />
+    <button primario @click="confirmou = true">Confirmar</button>
+    <!-- protegendo rotas -->
     <div class="rodape">
-        <h3>Curso Vue</h3>
+      <h3>Curso Vue</h3>
     </div>
   </div>
 </template>
@@ -15,11 +26,27 @@
 <script>
 export default {
     props: ['id'], 
-}
+    data() {
+        return {
+            confirmou: false
+        }
+    },
+    beforeRouteLeave(to, from, next) {
+        if(this.confirmou) {
+            //aqui tem a disposição todos os dados do componente tranquilamente pois já carregou.
+            next() } else {
+                    if(confirm('Tem certeza?')) {
+                        next()
+                    } else {
+                        next(false)
+                    }
+                }
+            }
+        }
 </script>
 
 <style>
-    #rodape {
-        margin-top: 1000px;
-    }
+#rodape {
+  margin-top: 1000px;
+}
 </style>
