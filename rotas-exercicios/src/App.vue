6329a736cb88a3753fbd8ad543d549a04228bca1
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2020-07-20 15:06:00 -0300

    264 Animando Transições de Rotas

--- rotas-exercicios/src/App.vue
+++ rotas-exercicios/src/App.vue
@@ -1,8 +1,12 @@
 <template>
 	<div id="app">
 		<h1>Rotas com VueRouter</h1>
-		<router-view name="menu"></router-view>
-		<router-view /> <!-- isso fará com que seja carregado os componentes do router.js, e usará os paths definidos de lá-->
+		<router-view name="menu"></router-view> <!-- o out-in eh pra as animacoes nao bugarem-->
+		<transition mode="out-in"
+			enter-active-class="animated rubberBand"
+			leave-active-class="animated rollOut">
+			<router-view /> <!-- isso fará com que seja carregado os componentes do router.js, e usará os paths definidos de lá-->
+		</transition>
 		<router-view name="menuInferior"></router-view>
 	</div>
 </template>
