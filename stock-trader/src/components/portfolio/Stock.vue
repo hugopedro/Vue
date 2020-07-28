161591604a7c37f977812f0ca897533c331e8e64
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2020-07-27 21:36:45 -0300

    309 Adicionando Validações

--- stock-trader/src/components/portfolio/Stock.vue
+++ stock-trader/src/components/portfolio/Stock.vue
@@ -12,10 +12,11 @@
         <v-card> <!--componente responsável por comprar a ação -->
             <v-container fill-height>
                 <v-text-field label="Quantidade" type="number"
+                :error="insufficientQuantity || !Number.isInteger(quantity)"
                     v-model.number="quantity"></v-text-field>
                 <v-btn class="blue darken-3 white--text"
-                :disabled="quantity <=0 || !Number.isInteger(quantity)"
-                    @click="sellStock ">Vender</v-btn>
+                :disabled="insufficientQuantity || quantity <=0 || !Number.isInteger(quantity)"
+                    @click="sellStock ">{{insufficientFunds ? 'Insuficiente' : 'Vender'}}</v-btn>
             </v-container>
         </v-card>
     </v-flex>
@@ -29,6 +30,11 @@ export default {
     data() {
         return {
             quantity: 0
+        }
+    },
+    computed: {
+        insufficientQuantity() {
+            return this.quantity > this.stock.quantity
         }
     },
     methods: {
