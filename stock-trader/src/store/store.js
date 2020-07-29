0d04fbab4aeef8b3413f05743c58a26783341abc
Hugo Pedro <hugopedro098@gmail.com>
2020-07-29 12:21:17 -0300

    315 Obtendo dados (Requisição GET)

--- stock-trader/src/store/store.js
+++ stock-trader/src/store/store.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import actions from './action'
 import stocks from './modules/stocks'
 import portfolio from './modules/portfolio'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+    actions,
     modules: {
         stocks,
         portfolio
