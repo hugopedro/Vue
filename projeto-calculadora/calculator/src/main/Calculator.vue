270b7bbd28ce687255b4c7cc34ef9aae30d39e9f
Hugo Pedro <hugopedro098@gmail.com>
2020-08-03 17:46:13 -0300

    331 Implementando lógica e conclusão

--- projeto-calculadora/calculator/src/main/Calculator.vue
+++ projeto-calculadora/calculator/src/main/Calculator.vue
@@ -1,48 +1,91 @@
 <template>
   <div class="calculator">
-      <Display value="1000" />
-      <Button label="AC" triple @onClick="clearMemory" />
-      <Button label="/" @onClick="setOperation"/>
-      <Button label="7" @onClick="addDigit" />
-      <Button label="8" @onClick="addDigit"/>
-      <Button label="9" @onClick="addDigit"/>
-      <Button label="*" @onClick="setOperation"/>
-      <Button label="4" @onClick="addDigit"/>
-      <Button label="5" @onClick="addDigit"/>
-      <Button label="6" @onClick="addDigit"/>
-      <Button label="-" @onClick="setOperation"/>
-      <Button label="1" @onClick="addDigit"/>
-      <Button label="2" @onClick="addDigit"/>
-      <Button label="3" @onClick="addDigit"/>
-      <Button label="+" @onClick="setOperation"/>
-      <Button label="0" double @onClick="addDigit"/>
-      <Button label="." @onClick="addDigit"/>
-      <Button label="=" @onClick="setOperation"/>
+    <Display :value="displayValue" />
+    <Button label="AC" triple @onClick="clearMemory" />
+    <Button label="/" operation @onClick="setOperation" />
+    <Button label="7" @onClick="addDigit" />
+    <Button label="8" @onClick="addDigit" />
+    <Button label="9" @onClick="addDigit" />
+    <Button label="*" operation @onClick="setOperation" />
+    <Button label="4" @onClick="addDigit" />
+    <Button label="5" @onClick="addDigit" />
+    <Button label="6" @onClick="addDigit" />
+    <Button label="-" operation @onClick="setOperation" />
+    <Button label="1" @onClick="addDigit" />
+    <Button label="2" @onClick="addDigit" />
+    <Button label="3" @onClick="addDigit" />
+    <Button label="+" operation @onClick="setOperation" />
+    <Button label="0" double @onClick="addDigit" />
+    <Button label="." @onClick="addDigit" />
+    <Button label="=" operation @onClick="setOperation" />
   </div>
 </template>
 
 <script>
+  import Button from '../components/Button'
+  import Display from '../components/Display'
 
-import Display from '../components/Display'
-import Button from '../components/Button'
-export default {
-    components: {Button, Display },
+  export default {
+    name: 'Calculator',
+    data() {
+      return {
+        displayValue: '0',
+        clearDisplay: false,
+        operation: null,
+        values: [ 0, 0 ],
+        current: 0
+      }
+    },
+    components: { Button, Display },
     methods: {
-        clearMemory() {
-            console.log('Limpar Memória')
-        },
-        setOperation(operation) {
-            console.log('Operação ' + operation)
-        },
-        addDigit(n) {
-            console.log('Digito' + n)
+      clearMemory() {
+        Object.assign( this.$data, this.$options.data() )
+      },
+      setOperation( operation ) {
+        if ( this.current === 0 ) {
+          this.operation = operation
+          this.current = 1
+          this.clearDisplay = true
+        } else {
+          const equals = operation === '='
+          const currentOperation = this.operation
+
+          try {
+            this.values[ 0 ] = eval(
+              `${ this.values[ 0 ] } ${ currentOperation } ${ this.values[ 1 ] }`
+            )
+          } catch ( e ) {
+            this.$emit( 'onError', e )
+          }
+          this.values[ 1 ] = 0
+          this.displayValue = this.values[ 0 ]
+          this.operation = equals ? null : operation
+          this.current = equals ? 0 : 1
+          this.clearDisplay = !equals
         }
-    }
-}
+
+      },
+      addDigit( n ) {
+        if ( n === '.' && this.displayValue.includes( '.' ) ) {
+          return
+        }
+
+        const clearDisplay = this.clearDisplay === '0' || this.clearDisplay
+        const currentValue = clearDisplay ? '' : this.displayValue
+        const displayValue = currentValue + n
+        this.displayValue = displayValue
+        this.clearDisplay = false
+
+        if ( n !== '.' ) {
+          this.values[ this.current ] = parseFloat( displayValue )
+        }
+      }
+    },
+  }
 </script>
 
 <style>
-.calculator {
+  .calculator {
     height: 320px;
     width: 235px;
     border-radius: 5px;
@@ -50,6 +93,6 @@ export default {
 
     display: grid;
     grid-template-columns: repeat(4, 25%);
-    grid-template-rows: 1fr 48px 48px 48px 48px 48px
-}
+    grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+  }
 </style>
